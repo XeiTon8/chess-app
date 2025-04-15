cbc2e8411e93137e48f3adbe7f2005ab6111799d
Vladyslav <vladyslavkhorunzhyi@gmail.com>
2025-04-15 21:08:22 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import './App.css'
+
+interface ICell {
+  x: string;
+  y: number | string;
+  color: string;
+  selected: boolean;
+}
+
+function App() {
+  
+  const [cells, setCells] = React.useState<ICell[][]>([])
+  const coordinates = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  React.useEffect(() => {
+  createBoard();
+  }, [])
+
+  const createBoard = () => {
+    const board: ICell[][] = [];
+    const reversedCoors = [...coordinates].reverse();
+
+    for (let i = reversedCoors.length - 1; i >=0; i--) {
+      const row: ICell[] = [];
+
+      for (let j = 0; j < numbers.length; j++) {
+          let newCell: ICell = {
+            x: reversedCoors[j],
+            y: numbers[i],
+            color: (i + j) % 2 == 0 ? "black" : "white",
+            selected: false,
+          }
+          row.push(newCell);
+      }
+
+    board.push(row);
+    }
+
+    setCells(board);
+  }
+
+  const selectCell = (x: string, y: number) => {
+    const newCells = [...cells].map((row) => {
+      return row.map((cell) => {
+        if (cell.x.includes(x) && cell.y === y) {
+            console.log(`X: ${cell.x}, Y: ${cell.y}`)
+            return {
+              ...cell,
+              selected: true,
+            }
+        } else {
+            return {
+              ...cell,
+              selected: false,
+            }
+        }
+      })
+  })
+    setCells(newCells);
+  }
+
+  const renderCells = () => {
+    return cells.map((row) => {
+      return row.map((cell, i) => {
+        return (
+          <div
+          key={i}
+          style={{
+            minWidth: "40px", height: "40px", 
+            backgroundColor: `${cell.selected ? "orange" : cell.color}`, 
+            color: `${cell.color.includes("white") ? "black" : "white"}`,
+            cursor: 'pointer',
+            }}
+            onClick={() => selectCell(String(cell.x), Number(cell.y))}
+            >{cell.x} {cell.y}</div>
+        )
+      })
+    })
+  }
+
+  return (
+    <div style={{maxWidth: "320px", display: "flex", flexWrap: "wrap"}}>
+      {renderCells()}
+    </div>
+  )
+}
+
+export default App
